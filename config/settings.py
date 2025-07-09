de37c9e6fb90149319e66a9664bb607ca7d18bea
JoyMuthoniNjeru <joy.njeru23@strathmore.edu>
2025-07-09 15:07:06 +0300

    setting up postgres

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ntsa_db',
+        'USER': 'postgres',
+        'PASSWORD': 'kemuga04!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
